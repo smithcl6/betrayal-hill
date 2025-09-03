8bc78fe1b10df0bdc96651ead138225cfd878443
Christopher Smith <smithcl6@alumni.vcu.edu>
2025-09-03 00:05:12 -0400

    Initial ASP.NET Core MVC project.

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace betrayal_hill.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
